1d9df64cbd915df02f14cf66dc674fb989d45700
Steven <stevenburris.jr@gmail.com>
2020-06-10 09:02:37 -0400

    Init Commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TempSensor2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //instantiate a temperature object
+            Temperature temperature = new Temperature();
+
+            //define the API url
+            string address = "http://localhost:22002/NeuLogAPI?GetSensorValue:[Temperature],[1]";
+
+            //intitiate the request
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(address);
+
+
+            //capture the response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+
+            //write the status code to the console if it's 200
+            Console.WriteLine(response.StatusCode);
+
+            //write the JSON string to the console
+            Console.WriteLine();
+
+
+            
+        }
+       
+        
+    }
+}
+
---
+++ Temperature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempSensor2
+{
+    class Temperature
+    {
+        private long Temp { get; set; }
+        private DateTime Time { get; set; }
+
+    }
+}
